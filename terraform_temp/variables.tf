f53e92fd32fd107178c66057e594cddfdc00ac19
Tony Najjar <tnajjar1@yahoo.com>
2026-07-28 14:55:08 -0400

    Rename Terraform directory to lowercase and complete networking module

---
+++ terraform_temp/variables.tf
@@ -0,0 +1,37 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project Name"
+  type        = string
+  default     = "three-tier"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_1" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_2" {
+  type    = string
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_1" {
+  type    = string
+  default = "10.0.11.0/24"
+}
+
+variable "private_subnet_2" {
+  type    = string
+  default = "10.0.12.0/24"
+}
